5da74b7085f3f43b80dbd11b5d2a56f721aac8ee
Chris Martin <bmg.martin08@gmail.com>
2020-10-23 21:41:34 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,3 +1,9 @@
 # Write your methods here
 def loop_message_five_times(string)
-  message = "hello "
+  counter = 0 
+  while string[counter] do
+    puts string[counter]
+    counter += 1 
+    string = "Hello World."
+  end
+end
